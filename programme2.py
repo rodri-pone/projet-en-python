b555d6bf8c89fee6d916880260647c4929353af7
rodri-pone <rodriguepone0@gmail.com>
2024-10-12 08:30:26 +0100

    Create programme2.py

---
+++ programme2.py
@@ -0,0 +1,6 @@
+#programme qui force l'utilisateur a entrer la lettre Q
+while 1:
+    lettre = input(" Tapez 'Q' pour quitter  ")
+    if lettre =='Q':
+        print("Fin de la boucle")
+        break
